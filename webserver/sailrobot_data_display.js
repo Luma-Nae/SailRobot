ce79ef66efcb46c6c92bfecc94cbb2b487fc504a
Bastian Starup Petersson <bastianpetersson@hotmail.com>
2020-03-20 10:39:16 +0100

    port 8080-3

--- webserver/sailrobot_data_display.js
+++ webserver/sailrobot_data_display.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 })
  
  http.listen(port, function() {
-    console.log('listening on *:3000');
+    console.log('listening on *:' + port);
  });
 
  fs.watch(dataFile, (event, filename) => {
