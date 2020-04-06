e1c151c0ebc98100c4fc96190f04613625bf44d1
Bastian Starup Petersson <bastianpetersson@hotmail.com>
2020-04-06 10:48:25 +0200

    IMUTEST print test 9

--- arduino_programs/IMUTEST/IMUTEST.ino
+++ arduino_programs/IMUTEST/IMUTEST.ino
@@ -1,16 +1,3 @@
-void setup() {
-  while (!Serial);
-
-  Serial.begin(115200);
-}
-
-
-void loop() {
-    Serial.print("test and test\n");
-    delay(500);
-}
-
-
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
