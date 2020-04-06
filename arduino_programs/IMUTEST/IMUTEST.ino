da4a0551d3fd152346f1c7dda68aee7b2f1e7fd9
Bastian Starup Petersson <bastianpetersson@hotmail.com>
2020-04-06 09:47:14 +0200

    IMUTEST print test

--- arduino_programs/IMUTEST/IMUTEST.ino
+++ arduino_programs/IMUTEST/IMUTEST.ino
@@ -1,16 +1,10 @@
-  // 9600 baud is the default rate for the Ultimate GPS
-  GPSSerial.begin(9600);
+void setup() {
+  while (!Serial);
+
+  Serial.begin(115200);
 }
 
 
 void loop() {
-//Serial.println(Serial.available());
-  if (Serial.available()) {
-    char c = Serial.read();
-    GPSSerial.write(c);
-  }
-  if (GPSSerial.available()) {
-    char c = GPSSerial.read();
-    Serial.write(c);
-  }
+    Serial.write("test");
 }
